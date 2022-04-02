8e22a9e74557fb251f58b208bd044aa3e68e2a00
Jong's <77009348+jongs9012@users.noreply.github.com>
2022-04-02 19:29:24 +0900

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,173 @@
+@echo off
+title [Á¤º¸Å·] ¸¶ÀÎÅ©·¡ÇÁÆ® 1.18.2 Paper ¹öÅ¶
+:main
+cls
+echo ¦È¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+echo ¦¢
+echo ¦¢     ¸¶ÀÎÅ©·¡ÇÁÆ® 1.18.2 Paper ¹öÅ¶
+echo ¦¢ 
+echo ¦¢   1. ¼­¹ö ±¸µ¿ÇÏ±â
+echo ¦¢ 
+echo ¦¢   2. ¿¡·¯ ¹ß»ý ½Ã ÇØ°á¹æ¹ý
+echo ¦¢
+echo ¦¢   3. IP È®ÀÎÇÏ±â
+echo ¦¢
+echo ¦¢   4. µµ¸ÞÀÎ ¹«·á ¸¸µé±â
+echo ¦¢
+echo ¦¢   5. ÇÃ·¯±×ÀÎ ´Ù¿î·Îµå
+echo ¦¢
+echo ¦¢   6. ¹öÅ¶ Á¾·á
+echo ¦¢
+echo ¦¢   *Á¤º¸Å· ºí·Î±× https://wjdqhzld.com
+echo ¦¢
+echo ¦Æ¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+
+set/p a=¹øÈ£¸¦ ÀûÀº ÈÄ Enter :
+if %a%==1 goto a
+if %a%==2 goto b
+if %a%==3 goto c
+if %a%==4 goto z
+if %a%==5 goto k
+if %a%==6 goto d
+
+:a
+cls
+echo ¦È¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+echo ¦¢
+echo ¦¢     ¸¶ÀÎÅ©·¡ÇÁÆ® 1.18.2 Paper ¹öÅ¶
+echo ¦¢ 
+echo ¦¢   1. ¼­¹ö ±¸µ¿ÇÏ±â - 1GB
+echo ¦¢ 
+echo ¦¢   2. ¼­¹ö ±¸µ¿ÇÏ±â - 2GB (±ÇÀå)
+echo ¦¢
+echo ¦¢   3. ¼­¹ö ±¸µ¿ÇÏ±â - 4GB
+echo ¦¢
+echo ¦¢   4. ¼­¹ö ±¸µ¿ÇÏ±â - 8GB
+echo ¦¢
+echo ¦¢   5. ¼­¹ö ±¸µ¿ÇÏ±â - 16GB
+echo ¦¢
+echo ¦¢   * Á¤º¸Å· ºí·Î±× https://wjdqhzld.com/
+echo ¦¢ 
+echo ¦Æ¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+
+set/p a=¹øÈ£¸¦ ÀûÀº ÈÄ Enter :
+
+if %a%==1 goto aa
+if %a%==2 goto bb
+if %a%==3 goto cc
+if %a%==4 goto dd
+if %a%==5 goto ddd
+
+:aa
+cls
+echo ¦È¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+echo ¦¢
+echo ¦¢      Á¤º¸Å·ÀÇ ¸¶ÀÎÅ©·¡ÇÁÆ® ¹öÅ¶À» ÀÌ¿ëÇØÁÖ¼Å¼­ °¨»çÇÕ´Ï´Ù
+echo ¦¢
+echo ¦¢      Á¤º¸Å·ÀÇ ¹öÅ¶ ±¸µ¿±â°¡ ¼­¹ö¿¡ ÀÌ¿ëÇÒ ÆÄÀÏ¸¦ ÀÚµ¿À¸·Î
+echo ¦¢      Å½ÁöÇÏ°í ÀÖ½À´Ï´Ù. Àá½Ã¸¸ ±â´Ù·ÁÁÖ¼¼¿ä 
+echo ¦¢
+echo ¦¢ * Âü°í»çÇ× : ¼­¹öÁ¾·á´Â stopÀÔ´Ï´Ù!!
+echo ¦¢
+echo ¦Æ¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+java -Xms1G -Xmx1G -jar paper-1.18.2.jar
+pause
+
+cls
+explorer "https://minepuls.tistory.com/"
+goto main
+
+:bb
+cls
+echo ¦È¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+echo ¦¢
+echo ¦¢      Á¤º¸Å·ÀÇ ¸¶ÀÎÅ©·¡ÇÁÆ® ¹öÅ¶À» ÀÌ¿ëÇØÁÖ¼Å¼­ °¨»çÇÕ´Ï´Ù
+echo ¦¢
+echo ¦¢      Á¤º¸Å·ÀÇ ¹öÅ¶ ±¸µ¿±â°¡ ¼­¹ö¿¡ ÀÌ¿ëÇÒ ÆÄÀÏ¸¦ ÀÚµ¿À¸·Î
+echo ¦¢      Å½ÁöÇÏ°í ÀÖ½À´Ï´Ù. Àá½Ã¸¸ ±â´Ù·ÁÁÖ¼¼¿ä 
+echo ¦¢
+echo ¦¢ * Âü°í»çÇ× : ¼­¹öÁ¾·á´Â stopÀÔ´Ï´Ù!!
+echo ¦¢
+echo ¦Æ¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+java -Xms1G -Xmx2G -jar paper-1.18.2.jar
+pause
+
+cls
+explorer "https://minepuls.tistory.com/"
+goto main
+
+:cc
+cls
+echo ¦È¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+echo ¦¢
+echo ¦¢      Á¤º¸Å·ÀÇ ¸¶ÀÎÅ©·¡ÇÁÆ® ¹öÅ¶À» ÀÌ¿ëÇØÁÖ¼Å¼­ °¨»çÇÕ´Ï´Ù
+echo ¦¢
+echo ¦¢      Á¤º¸Å·ÀÇ ¹öÅ¶ ±¸µ¿±â°¡ ¼­¹ö¿¡ ÀÌ¿ëÇÒ ÆÄÀÏ¸¦ ÀÚµ¿À¸·Î
+echo ¦¢      Å½ÁöÇÏ°í ÀÖ½À´Ï´Ù. Àá½Ã¸¸ ±â´Ù·ÁÁÖ¼¼¿ä 
+echo ¦¢
+echo ¦¢ * Âü°í»çÇ× : ¼­¹öÁ¾·á´Â stopÀÔ´Ï´Ù!!
+echo ¦¢
+echo ¦Æ¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+java -Xms1G -Xmx4G -jar paper-1.18.2.jar
+pause
+
+cls
+explorer "https://minepuls.tistory.com/"
+goto main
+
+:dd
+cls
+echo ¦È¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+echo ¦¢
+echo ¦¢      Á¤º¸Å·ÀÇ ¸¶ÀÎÅ©·¡ÇÁÆ® ¹öÅ¶À» ÀÌ¿ëÇØÁÖ¼Å¼­ °¨»çÇÕ´Ï´Ù
+echo ¦¢
+echo ¦¢      Á¤º¸Å·ÀÇ ¹öÅ¶ ±¸µ¿±â°¡ ¼­¹ö¿¡ ÀÌ¿ëÇÒ ÆÄÀÏ¸¦ ÀÚµ¿À¸·Î
+echo ¦¢      Å½ÁöÇÏ°í ÀÖ½À´Ï´Ù. Àá½Ã¸¸ ±â´Ù·ÁÁÖ¼¼¿ä 
+echo ¦¢
+echo ¦¢ * Âü°í»çÇ× : ¼­¹öÁ¾·á´Â stopÀÔ´Ï´Ù!!
+echo ¦¢
+echo ¦Æ¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+java -Xms2G -Xmx8G -jar paper-1.18.2.jar
+pause
+
+:ddd
+cls
+echo ¦È¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+echo ¦¢
+echo ¦¢      Á¤º¸Å·ÀÇ ¸¶ÀÎÅ©·¡ÇÁÆ® ¹öÅ¶À» ÀÌ¿ëÇØÁÖ¼Å¼­ °¨»çÇÕ´Ï´Ù
+echo ¦¢
+echo ¦¢      Á¤º¸Å·ÀÇ ¹öÅ¶ ±¸µ¿±â°¡ ¼­¹ö¿¡ ÀÌ¿ëÇÒ ÆÄÀÏ¸¦ ÀÚµ¿À¸·Î
+echo ¦¢      Å½ÁöÇÏ°í ÀÖ½À´Ï´Ù. Àá½Ã¸¸ ±â´Ù·ÁÁÖ¼¼¿ä 
+echo ¦¢
+echo ¦¢ * Âü°í»çÇ× : ¼­¹öÁ¾·á´Â stopÀÔ´Ï´Ù!!
+echo ¦¢
+echo ¦Æ¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡
+java -Xms4G -Xmx16G -jar paper-1.18.2.jar
+pause
+
+cls
+explorer "https://minepuls.tistory.com/"
+goto main
+
+:b
+cls
+explorer "https://wjdqhzld.com/¸¶ÀÎÅ©·¡ÇÁÆ®-¸ÖÆ¼-Á¢¼Ó-¿À·ù"
+goto main
+
+:c
+cls
+explorer "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=³»¾ÆÀÌÇÇ"
+goto main
+
+:z
+cls
+explorer "https://wjdqhzld.com/µµ¸ÞÀÎ-¹«·á-¸¸µé±â"
+goto main
+
+:k
+cls
+explorer "https://wjdqhzld.com/category/minecraft/server/plugin/"
+goto main
+
+:d
+exit
